13f9783283bfea05efc056147c4331d6716625fe
Augusto <augustofelipemf45@gmail.com>
2025-07-03 20:22:01 -0300

    finish adjust for challenge 4

--- desafio4/assets/js/index.js
+++ desafio4/assets/js/index.js
@@ -1,59 +1,65 @@
-const form = document.querySelector('form');
-const nome = form.querySelector('#nome');
-const email = form.querySelector('#email'); 
-const vagasEl = document.querySelector('.vagas-restantes');
-const btn = form.querySelector('button');
-const escassez = document.querySelector('.escassez');
+function meuEscopo(){
 
-setInterval(() => {
+    const sectionCta = document.querySelector('section.cta')
+    const form = document.querySelector('form');
+    const nome = form.querySelector('#nome');
+    const email = form.querySelector('#email');
+    const vagasEl = document.querySelector('.vagas-restantes');
+    const btn = form.querySelector('button');
+    const escassez = document.querySelector('.escassez');
+    
+    setInterval(() => {
     escassez.classList.toggle('hover');
 }, 800)
 
-if(!localStorage.getItem('vagas')){
+if (!localStorage.getItem('vagas')) {
     localStorage.setItem('vagas', 10);
 }
 
-function atualizarVagas(){
+function atualizarVagas() {
     let vagas = Number(localStorage.getItem('vagas'));
     vagasEl.innerHTML = `${vagas} vagas restantes`;
-
+    
     mentoriaFechada();
 }
 atualizarVagas();
 
-form.addEventListener('submit', e =>{
+form.addEventListener('submit', e => {
     e.preventDefault();
     
     verificandoVagas();
 })
 
-function limpaInput(){
+function limpaInput() {
     nome.value = '';
     email.value = '';
     nome.focus();
 }
 
-function verificandoVagas(){
-let vagas = Number(localStorage.getItem('vagas'));
-
-    if(vagas>0){
+function verificandoVagas() {
+    let vagas = Number(localStorage.getItem('vagas'));
+    
+    if (vagas > 0) {
         vagas--;
         localStorage.setItem('vagas', vagas.toString());
+        
+        salvarInscrito(nome.value, email.value);
+        
         alert('Formulário enviado com sucesso!');
         atualizarVagas();
         limpaInput();
-    }  else{
+    } else {
         alert('[ERRO]Desculpe, todas as vagas foram preenchidas');
         mentoriaFechada();
     }
-    
+
     
 }
 
-function mentoriaFechada(){
+function mentoriaFechada() {
     let vagas = Number(localStorage.getItem('vagas'));
     
-    if(vagas == 0) {
+    if (vagas === 0) {
         vagasEl.style.color = 'red';
         // Estilo Vermelho
         vagasEl.style.color = 'red';
@@ -66,15 +72,68 @@ function mentoriaFechada(){
         
         // Efeito hover no color red
         form.classList.add('mentoria-fechada');
+        
+        
+        divExtra();
     }
 }
 
-document.addEventListener('keydown', function(e){
-    if(e.ctrlKey && e.key === 'r'){
-        localStorage.removeItem('vagas');
+function divExtra() {
+    setTimeout(() => {
+        const div = document.createElement('div');
+        div.classList.add('div-extra');
+        div.innerText = 'As vagas foram encerradas.\nAguarde uma nova oportunidade';
+        sectionCta.appendChild(div);
+        form.style.filter = "blur(3px)";
+        form.style.pointerEvents = 'none';
+        contemplados(div);
+
+        
+        
+    }, 1000);
+}
+
+function contemplados(div) {
+    const btnInscritos = document.createElement('button');
+    btnInscritos.innerText = 'PESSOAS INSCRITAS';
+    
+    btnInscritos.addEventListener('click', () => {
+        // const inscritos = JSON.parse(localStorage.getItem('inscritos')) || [];
+        const inscritos = [];
+
+        if(inscritos.length === 0){
+            alert('Nenhuma pessoa inscrita ainda.');
+            return;
+        } else{
+            window.location.href = './agradecimento.html';
+        }
+    })
+    div.appendChild(btnInscritos);
+}
+
+
+
+
+document.addEventListener('keydown', function (e) {
+    if (e.ctrlKey && e.key === 'r') {
+        localStorage.setItem('vagas', 1);
         alert('Vagas resetadas.');
         location.reload();
         
         form.classList.remove('mentoria-fechada');
     }
 });
+
+function salvarInscrito(nome, email) {
+    let inscritos = JSON.parse(localStorage.getItem('inscritos')) || [];
+    inscritos.push({ nome, email });
+    localStorage.setItem('inscritos', JSON.stringify(inscritos));
+}
+
+function mudarPagina() {
+    setTimeout(() => {
+        window.location.href = './agradecimento.html';
+    }, 5000)
+}
+}
+meuEscopo();
